e4589476d680410edff4c5dbdb85976c45e8ed7b
Nafisa Yeasmin <42702148+nafisatonni@users.noreply.github.com>
2023-08-08 01:06:51 +0600

    Create README.md

---
+++ Day2/operators.py
@@ -0,0 +1,45 @@
+#Arithmetic Operator
+print("Arithmetic Operator")
+a = 5
+b = 2
+
+add = a+b #additon of a ,b
+print(add)
+
+sub = a-b
+print(sub) #Subtraction
+
+mul = a*b
+print(mul) #multiplication
+
+print(a/b)  #2.5
+print(a//b) #2 quotient
+print(a % b) #1 reminder
+print(a**b) #5^2 / 5**2 = 25
+
+
+print("relational Operators")
+#relational Operators
+# > < >= <= !=
+#relationals operators always return a boolean valur True/False
+
+a = 5
+b = 10
+
+print(a>b) #false
+print(a<b) #True
+print(a==b) #False
+print(a>=b) #false
+print(a<=b) #true
+print(a!=b) #true
+
+print("Logical Operators")
+#Logical Operators
+#   and or not
+
+ x = True
+ y = False
+
+print( x and y)
+print( x or y)
+print( not x)
